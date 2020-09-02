efba19eeac214fe25809cec23ec7ca257ff31dd7
wangjing12345678 <121897354@qq.com>
2020-09-02 14:21:21 +0800

    打包普通该项目 路径问题注意

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-// require('./static/image/00-go.png');
-// require('./static/index.css');
+require('../static/image/00-go.png');
+require('../static/index.css');
 require('./another');
-var $ = require('./static/js/jquery-3.3.1')
+var $ = require('../static/js/jquery-3.3.1')
 
 var m = require('./er');
 var param = new m.getparam();
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,128 @@
 const path = require('path');
 const HTMLPlugin = require('html-webpack-plugin');
+// let extractPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
-// const isDev = process.env.NODE_ENV === 'development';
 let env = process.env.NODE_ENV;
 console.log(env);
-const config = {
-    mode: 'production',
-    entry: {
-        index: path.join(__dirname, 'index.js'),
-        er: path.join(__dirname, 'er.js'),
-    },
-    output: {
-        filename: '[name].js', // 文件输出
-        path: path.join(__dirname, '/dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/, // 处理css文件
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(gif|jpg|jpeg|png|svg)$/, // 处理图片文件
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 1024,
-                            name: 'static/image/[name].[ext]',
+
+var config = ''
+if (env == 'development') {
+    config = {
+        mode: 'development',
+        entry: {
+            index: path.join(__dirname, 'js/index.js'),
+            er: path.join(__dirname, 'js/er.js'),
+        },
+        output: {
+            publicPath: '/',
+            filename: 'js/[name].js', // 文件输出
+            path: path.join(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/, // 处理css文件
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(gif|jpg|jpeg|png|svg)$/, // 处理图片文件
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 1024,
+                                name: 'static/image/[name].[ext]',
+                            }
                         }
-                    }
-                ]
-            }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HTMLPlugin({ // 打包输出HTML
+                filename: 'views/index.html',
+                template: './views/index.html',
+                minify: { // 压缩HTML文件
+                    removeComments: true, // 移除HTML中的注释
+                    collapseWhitespace: true, // 删除空白符与换行符
+                    minifyCSS: true// 压缩内联css
+                },
+                chunks: ['./js/index']
+            }),
+            new HTMLPlugin({ // 打包输出HTML
+                filename: 'views/second.html',
+                template: './views/second.html',
+                minify: { // 压缩HTML文件
+                    removeComments: true, // 移除HTML中的注释
+                    collapseWhitespace: true, // 删除空白符与换行符
+                    minifyCSS: true// 压缩内联css
+                },
+                chunks: ['./js/er']
+            }),
         ]
-    },
-    plugins: [
-        // new webpack.DefinePlugin({
-        //     "process.env.NODE_ENV": JSON.stringify('production/development')
-        // }),
-        new HTMLPlugin({ // 打包输出HTML
-            filename: 'index.html',
-            template: 'index.html',
-            minify: { // 压缩HTML文件
-                removeComments: true, // 移除HTML中的注释
-                collapseWhitespace: true, // 删除空白符与换行符
-                minifyCSS: true// 压缩内联css
-            },
-            chunks: ['./index']
-        }),
-        new HTMLPlugin({ // 打包输出HTML
-            filename: 'second.html',
-            template: 'second.html',
-            minify: { // 压缩HTML文件
-                removeComments: true, // 移除HTML中的注释
-                collapseWhitespace: true, // 删除空白符与换行符
-                minifyCSS: true// 压缩内联css
-            },
-            chunks: ['./er']
-        }),
-    ]
+    }
+} else {
+    config = {
+        mode: 'production',
+        entry: {
+            index: path.join(__dirname, 'js/index.js'),
+            er: path.join(__dirname, 'js/er.js'),
+        },
+        output: {
+            publicPath: '../',
+            filename: 'js/[name].js', // 文件输出
+            path: path.join(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/, // 处理css文件
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(gif|jpg|jpeg|png|svg)$/, // 处理图片文件
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                limit: 1024,
+                                name: 'static/image/[name].[ext]',
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HTMLPlugin({ // 打包输出HTML
+                filename: 'views/index.html',
+                template: './views/index.html',
+                minify: { // 压缩HTML文件
+                    removeComments: true, // 移除HTML中的注释
+                    collapseWhitespace: true, // 删除空白符与换行符
+                    minifyCSS: true// 压缩内联css
+                },
+                chunks: ['./js/index']
+            }),
+            new HTMLPlugin({ // 打包输出HTML
+                filename: 'views/second.html',
+                template: './views/second.html',
+                minify: { // 压缩HTML文件
+                    removeComments: true, // 移除HTML中的注释
+                    collapseWhitespace: true, // 删除空白符与换行符
+                    minifyCSS: true// 压缩内联css
+                },
+                chunks: ['./js/er']
+            }),
+        ]
+    }
 }
+
+
 module.exports = config;
